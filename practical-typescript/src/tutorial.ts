6fe2143e36637552f7b03a08668097eedf7f3621
Shariar Islam <shariarn85@gmail.com>
2024-04-01 00:53:57 +0600

    Basic start

---
+++ practical-typescript/src/tutorial.ts
@@ -0,0 +1,18 @@
+/* let awesomeName: string = "shakeAndBake";
+
+awesomeName = "new name";
+// awesomeName = 20; // not possible for number type. awesomeName only takes type string!
+
+let amount: number = 20;
+
+amount = 12 - 1;
+// amount = "3 Pants"; // not possible for string type. amount only takes type number! */
+
+let gretings: string = "Hello";
+gretings = "good afternoon";
+
+let age: number = 20;
+age = age + 5;
+
+// gretings = 24; // not possible
+// age = "-5"; // not possible
