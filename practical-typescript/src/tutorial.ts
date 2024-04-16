0ab75d5748e0a217ad5ecd1e94daa90d3b371c8c
Shariar Islam <shariarn85@gmail.com>
2024-04-17 00:09:12 +0600

    optional value type added

--- practical-typescript/src/tutorial.ts
+++ practical-typescript/src/tutorial.ts
@@ -189,5 +189,11 @@ if (isNameInList(nameToCheck)) {
 }
  */
 
-
 // Functions - Optional and Default Parameters
+
+function calculatePrice(price: number, discount?: number) {
+  return price - (discount || 0);
+}
+
+const priceAfterDiscount = calculatePrice(100, 20);
+console.log(priceAfterDiscount); // output 80
