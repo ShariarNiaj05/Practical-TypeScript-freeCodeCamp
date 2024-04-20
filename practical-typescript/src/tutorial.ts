9a277d207b0548fd6d1f757dc9fea19b7374e805
Shariar Islam <shariarn85@gmail.com>
2024-04-21 00:50:05 +0600

    simple

--- practical-typescript/src/tutorial.ts
+++ practical-typescript/src/tutorial.ts
@@ -201,7 +201,7 @@ console.log(priceAfterDiscount); // output 80
 let priceWithoutDiscount = calculatePrice(300);
 console.log(priceWithoutDiscount); // Output: 300 */
 
-function calculateScore(initialScore: number, penaltyPoints: number = 0) {
+/* function calculateScore(initialScore: number, penaltyPoints: number = 0) {
   return initialScore - penaltyPoints;
 }
 
@@ -209,4 +209,4 @@ let scoreAfterPenalty = calculateScore(100, 20);
 console.log(scoreAfterPenalty); // Output: 80
 
 let scoreWithoutPenalty = calculateScore(300);
-console.log(scoreWithoutPenalty); // Output: 300
+console.log(scoreWithoutPenalty); // Output: 300 */
